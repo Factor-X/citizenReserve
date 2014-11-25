56b7bfdfe60ec8541436c70b6ee8bafb5df0351b
jcarton <jerome.carton@factorx.eu>
2014-11-25 16:30:53 +0100

    Added Questions codes

--- app/eu/factorx/citizenReserve/dto/SurveyDTO.java
+++ app/eu/factorx/citizenReserve/dto/SurveyDTO.java
@@ -8,4 +8,22 @@ public class SurveyDTO {
 
     private List<AnswerDTO> answers;
 
+    public SurveyDTO() {
+    }
+
+    public AccountDTO getAccount() {
+        return account;
+    }
+
+    public void setAccount(AccountDTO account) {
+        this.account = account;
+    }
+
+    public List<AnswerDTO> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<AnswerDTO> answers) {
+        this.answers = answers;
+    }
 }
--- app/eu/factorx/citizenReserve/model/QuestionCode.java
+++ app/eu/factorx/citizenReserve/model/QuestionCode.java
@@ -1,6 +1,68 @@
 package eu.factorx.citizenReserve.model;
 
 public enum QuestionCode {
-    Q1
-    // TODO Add question keys...
+    Q1300(),
+    Q1400(),
+    Q1500(),
+    Q1110(1300),
+    Q1120(2000),
+    Q1130(3500),
+    Q1600(70),
+    Q1210(250),
+    Q1900(),
+    Q1160(1100),
+    Q1220(60),
+    Q1230(40),
+    Q1700(180),
+    Q1800(),
+    Q2010(700),
+    Q2020(600),
+    Q2030(1200),
+    Q2040(1800),
+    Q1235(4000),
+    Q1140(3500),
+    Q1150(2000),
+    Q3210(),
+    Q3211(),
+    Q3110(),
+    Q3120(),
+    Q3130(),
+    Q3310(),
+    Q3320(),
+    Q3330(),
+    Q3410(),
+    Q3420(),
+    Q3510(),
+    Q3530(),
+    Q3610(),
+    Q3620(),
+    Q3630(),
+    Q3631(),
+    Q3640(),
+    Q3810(),
+    Q3710(),
+    Q3711(),
+    Q3720(),
+    Q3730(),
+    Q3740(),
+    Q3741(),
+    Q3750(),
+    Q3760();
+
+    private Integer nominalPower = null;
+
+    QuestionCode() {
+    }
+
+    QuestionCode(Integer nominalPower) {
+        this.nominalPower = nominalPower;
+    }
+
+    public Integer getNominalPower() {
+        return nominalPower;
+    }
+
+    public void setNominalPower(Integer nominalPower) {
+        this.nominalPower = nominalPower;
+    }
 }
