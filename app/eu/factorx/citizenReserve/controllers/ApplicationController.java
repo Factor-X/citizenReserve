ca2aecd3fc9dfe7392f77ead915ec180c8d93cfb
jcarton <jerome.carton@factorx.eu>
2014-11-26 14:39:22 +0100

    Survey entity, dao, tests + translations

--- app/eu/factorx/citizenReserve/controllers/ApplicationController.java
+++ app/eu/factorx/citizenReserve/controllers/ApplicationController.java
@@ -3,9 +3,6 @@ package eu.factorx.citizenReserve.controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-/**
- * Created by florian on 18/11/14.
- */
 public class ApplicationController extends Controller {
 
     public Result index(){
