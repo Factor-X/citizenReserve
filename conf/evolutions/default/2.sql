992f341bc1e60abf098386c4bf1797e6dd5afa72
jcarton <jerome.carton@factorx.eu>
2015-01-05 10:57:37 +0100

    Put DB adaptations (for legacy data) in same sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,8 +2,10 @@
 # --- !Ups
 
 ALTER TABLE accounts ADD COLUMN legacy_account_power_reduction double precision;
+ALTER TABLE accounts ADD COLUMN password_to_change boolean;
 
 # --- !Downs
 
 ALTER TABLE accounts DROP COLUMN legacy_account_power_reduction;
+ALTER TABLE accounts DROP COLUMN password_to_change;
 
--- conf/evolutions/default/3.sql
+++
@@ -1,9 +0,0 @@
-
-# --- !Ups
-
-ALTER TABLE accounts ADD COLUMN password_to_change boolean;
-
-# --- !Downs
-
-ALTER TABLE accounts DROP COLUMN password_to_change;
-
